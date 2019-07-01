da021900e0d82b60c5eff1ebdfe4d8508e25712d
Ivan <ivankevinbautista@gmail.com>
2019-07-01 14:54:27 +0800

    added logo folder, and logo.js and made other edits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/Logo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Navigation />
+      <Logo />
+      {/*
+          <ImageLinkForm />
+          <FaceRecognition />} */}
     </div>
   );
 }
