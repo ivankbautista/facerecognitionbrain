2147cdd5e1745c966227c7f10be4c45e271c0594
Ivan <ivankevinbautista@gmail.com>
2019-07-08 14:13:43 +0800

    added loadUser to Signin, and added code to <Rank> that updates the rank with the states of the user

--- src/App.js
+++ src/App.js
@@ -34,14 +34,25 @@ class App extends Component {
       imageUrl:'',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+            id: '',
+            name: '',
+            email: '',
+            entries: 0,
+            joined: ''
+      }
     }
   }
 
-  componentDidMount() {
-    fetch('http://localhost:3000')
-      .then(response => response.json())
-      .then(console.log)
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
 
@@ -96,7 +107,7 @@ class App extends Component {
        { route === 'home' 
         ? <div>
             <Logo />
-            <Rank />
+            <Rank name ={this.state.user.name} entries = {this.state.user.entries}/>
             <ImageLinkForm 
               onInputChange={this.onInputChange} 
               onButtonSubmit={this.onButtonSubmit}
@@ -105,8 +116,8 @@ class App extends Component {
           </div>
           : (
             route === 'signin' 
-            ? <Signin onRouteChange={this.onRouteChange}/>
-            : <Register onRouteChange={this.onRouteChange}/>
+            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
           )
         }
       </div>
